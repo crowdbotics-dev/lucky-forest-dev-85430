5f6b4f32ec1dd21c7792882174d9be57652ef9b9
Crowdbotics <engineering@crowdbotics.com>
2023-05-22 23:08:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-forest-dev-85430",
+  url: "https://lucky-forest-dev-85430.botics.co",
+  api: "https://lucky-forest-dev-85430.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
